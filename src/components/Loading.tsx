b847969606ff9db137aff84f6593fe9418cfbe81
Ayush kumar <kumarayushkumar@protonmail.com>
2023-11-14 18:15:00 +0530

    code refac and some pages

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Loading...</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/interface.ts
+++ src/interface.ts
@@ -1,24 +1,24 @@
 enum MetalCategory {
-  gold = 'gold',
-  silver = 'silver',
-  platinum = 'platinum',
-  roseGold = 'rose gold', // Rose gold is an alloy made from a combination of pure gold and copper. 75 percent pure gold to 25 percent copper, which makes 18k rose gold.
+  gold = "gold",
+  silver = "silver",
+  platinum = "platinum",
+  roseGold = "rose gold", // Rose gold is an alloy made from a combination of pure gold and copper. 75 percent pure gold to 25 percent copper, which makes 18k rose gold.
 }
 
-type gender = 'M' | 'F';
+type gender = "M" | "F";
 
 enum ProductCategory {
-  ring = 'ring',
-  necklace = 'necklace',
-  bracelet = 'bracelet',
-  earring = 'earring',
-  chain = 'chain',
-  pendant = 'pendant',
-  kada = 'kada',
-  coin = 'coin',
-  bangle = 'bangle',
-  mangalsutra = 'mangalsutra',
-  payal = 'payal',
+  ring = "ring",
+  necklace = "necklace",
+  bracelet = "bracelet",
+  earring = "earring",
+  chain = "chain",
+  pendant = "pendant",
+  kada = "kada",
+  coin = "coin",
+  bangle = "bangle",
+  mangalsutra = "mangalsutra",
+  payal = "payal",
 
 }
 
@@ -33,7 +33,7 @@ export interface IProduct {
   gender: gender;
   description: string;
   price: number;
-  image: string;
+  img: string;
   color: string;
   productSize?: number;
 
@@ -45,7 +45,7 @@ export interface IProduct {
   labourCharge?: number; // per gram
   goldPurity?: number; // KT
   goldWastage?: number;
-  
+
   grossWeight: number;
   goldWeight?: number;
   platinumWeight?: number;
